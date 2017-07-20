af9dbad85a92da540a7a37e8e17873bc59d8e529
John Leyden <johnjl712@gmail.com>
2017-07-20 01:10:35 -0700

    finished React environment

---
+++ src/layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Hello World, I am John</h1>
+                <h2>Prepare to be changed</h2>
+        </div>);
+    }
+}
