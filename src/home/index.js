7d1b910b4784c504b51f8de2e7a99456136752ef
John Leyden <johnjl712@gmail.com>
2017-08-19 11:38:47 -0700

    homepage dimensions and dragging

--- src/home/index.js
+++ src/home/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import ElementPan from 'react-element-pan'
+
+import lakePano from './lakePano.jpg';
+
 
 export default class Home extends React.Component {
 
 	render() {
+		const height = this.props.data.contentHeight
+		console.log(height)
 		return (
-			<h1>Home</h1>
+			<ElementPan startX={600} startY={100} height={height}>
+				<img src={ lakePano } height={height + 120}/>
+			</ElementPan>
 		)
 	}
 }
