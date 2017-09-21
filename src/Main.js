1d7e7fd9c4224f9ec30511c3810df2f171b6d11a
John Leyden <johnjl712@gmail.com>
2017-09-20 18:05:47 -0700

    preliminary routing

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import NavBar from './NavBar.js';
 import Home from './home'
-import Projects from './projects'
 import Blog from './blog'
 import About from './about'
 import Contact from './contact'
+import Projects from './projects'
+import caAirPollution from './projects/caAirPollution'
 
 const NAV_BAR_HEIGHT = 80
 
@@ -20,12 +22,13 @@ export default class Main extends React.Component {
   render() {
     return (
       <MuiThemeProvider style = {{Margin: 0}}>
-        <Router>
+        <Router history={createBrowserHistory()}>
           <div>
             <Route path="/" component={withDimensions(NavBar)}/>
             <div>
               <Route exact path="/" component={withDimensions(Home)}/>
-              <Route path="/projects" component={withDimensions(Projects)}/>
+              <Route exact path="/projects" component={withDimensions(Projects)}/>
+              <Route exact path="/ca-air-pollution" component={withDimensions(caAirPollution)}/>
               <Route path="/blog" component={withDimensions(Blog)}/>
               <Route path="/about" component={withDimensions(About)}/>
               <Route path="/contact" component={withDimensions(Contact)}/>
--- src/projects/caAirPollution/index.js
+++ src/projects/caAirPollution/index.js
@@ -1,3 +1,10 @@
-/**
- * Created by jleyden on 9/17/17.
- */
+import React from 'react';
+
+export default class caAirPollution extends React.Component {
+
+	render() {
+		return (
+			<h1>CA Air Pollution</h1>
+		)
+	}
+}
