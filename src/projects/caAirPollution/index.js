aac6583003560cc65c7edf09d0b40301b5220221
John Leyden <johnjl712@gmail.com>
2017-10-07 13:13:44 -0700

    preliminary fresno chart

--- src/projects/caAirPollution/index.js
+++ src/projects/caAirPollution/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import {
 	BarChart, Bar, XAxis, YAxis, CartesianGrid,
 	Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import json2csv from 'json2csv'
+import { CSVLink } from 'react-csv'
+import { keys, nth } from 'lodash'
+import RaisedButton from 'material-ui/RaisedButton';
 
 import getData from './data/fresno'
+
+const textStyle1 = {
+	color: '#4DD0E1',
+	fontFamily: 'Helvetica',
+	textAlign: 'Center'
+}
 
 export default class caAirPollution extends React.Component {
 
 	render() {
 		const data = getData()
+		const fields = keys(nth(data))
+		const dataCSV = json2csv({data: data, fields: fields})
 		return (
 			<div>
-				<h1 style={{color: '#4DD0E1'}}>Fresno County Air Pollution</h1>
+				<h1 style={textStyle1}>Fresno County Air Pollution, 2012</h1>
 				<ResponsiveContainer width={'100%'} height={500}>
 					<BarChart data={data}
 					          margin={{top: 10, right: 30, left: 0, bottom: 0}}>
@@ -19,7 +31,7 @@ export default class caAirPollution extends React.Component {
 						<YAxis/>
 						<CartesianGrid strokeDasharray="3 3"/>
 						<Tooltip/>
-						<Legend />
+						<Legend style={textStyle1}/>
 						<Bar dataKey="TOG" stackId="a" fill="#f44336" />
 						<Bar dataKey="ROG" stackId="a" fill="#9C27B0" />
 						<Bar dataKey="COT" stackId="a" fill="#3F51B5" />
@@ -31,6 +43,13 @@ export default class caAirPollution extends React.Component {
 						<Bar dataKey="NH3" stackId="a" fill="#FF5722" />
 					</BarChart>
 				</ResponsiveContainer>
+				<CSVLink data={dataCSV}
+				         filename='fresno-emissions.csv'
+				         style={textStyle1}>
+					<RaisedButton label='Download CSV'
+					              style={textStyle1}
+					              primary={true}/>
+				</CSVLink>
 			</div>
 		)
 	}
